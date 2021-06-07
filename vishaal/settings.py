da75bfc76d720f9d73b425ec9b154496955ecf3f
Vishaal7 <72020853+Vishaal7@users.noreply.github.com>
2021-06-07 22:24:26 +0530

    Update settings.py

--- vishaal/settings.py
+++ vishaal/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[ os.path.join(BASE_DIR,'static')]
 STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
